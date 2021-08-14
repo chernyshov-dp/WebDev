76970e60da75f8293ce91803776a0c5d48167ee7
Daniil Chernyshov <chernyshoff.dp@gmail.com>
2021-08-15 00:35:12 +0300

    JS basics added

---
+++ JavaScript/basics.js
@@ -0,0 +1,45 @@
+alert("Hello world!")
+
+// anything here is a comment
+
+// BASIC DATA TYPES
+10
+20.2
+-12.5
+
+// Strings
+"Hello world"
+"10"
+
+// Booleans
+true
+false
+undefined
+null
+
+"JS " + "is cool"
+"Django".length
+"x x".length
+
+"hello"[0];
+
+// VARIABLES
+// var varName = value; in camelCase!
+
+var bankAccount = 100;
+bankAccount;
+var deposit = 50;
+var total = bankAccount + deposit;
+total;
+
+var greeting = "Welcome back ";
+var first_name = "Daniil";
+alert(greeting + first_name);
+
+var bonus = null;
+
+console.log("Hey I'm in the console");
+prompt("Something");
+
+var age = prompt("Enter your age:");
+age
