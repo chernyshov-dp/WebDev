1055e1c1f8c0d034a87de1cef69bccef56b53d3e
Daniil Chernyshov <chernyshoff.dp@gmail.com>
2021-08-16 14:31:06 +0300

    Renamed JS section + pt3 exercise

---
+++ JavaScript_LEVEL_ONE/Part3_exercise.js
@@ -0,0 +1,4 @@
+var lbs = prompt("What is the weight in pounds (lbs)?");
+var kgs = lbs * 0.454;
+alert("That is: " + kgs + " kilograms");
+console.log("Conversion completed");
---
+++ JavaScript_LEVEL_ONE/myscript.js
@@ -0,0 +1,4 @@
+alert("Welcome to your bank");
+var deposit = prompt("How much would you like to deposit today?");
+alert("You've deposited: " + deposit);
+console.log("You are a cool person!");
