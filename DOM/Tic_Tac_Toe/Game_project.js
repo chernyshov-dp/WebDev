33b4682d98d0502a4077cf19e17f2f9efe670196
Daniil Chernyshov <chernyshoff.dp@gmail.com>
2021-08-22 16:10:58 +0300

    Completed game project

---
+++ DOM/Tic_Tac_Toe/Game_project.js
@@ -0,0 +1,26 @@
+var restartButton = document.querySelector("#restart")
+var squares = document.querySelectorAll("td")
+
+function clearTable() {
+    for (var i = 0; i < squares.length; i++) {
+        squares[i].textContent = "";
+    }
+}
+
+restartButton.addEventListener("click", clearTable)
+
+cellOne = document.querySelector("#one");
+
+function changeMarker() {
+    if (this.textContent == "") {
+        this.textContent = "X";
+    } else if (this.textContent == "X") {
+        this.textContent = "O";
+    } else {
+        this.textContent = "";
+    }
+}
+
+for (var i = 0; i < squares.length; i++){
+    squares[i].addEventListener("click", changeMarker)
+}
