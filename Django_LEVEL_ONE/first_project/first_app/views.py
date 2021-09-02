f33b4ee9c57e5a0d5c5b419ac4ea68f8697435c8
Daniil Chernyshov <chernyshoff.dp@gmail.com>
2021-09-02 21:25:12 +0300

    add templates

--- Django_LEVEL_ONE/first_project/first_app/views.py
+++ Django_LEVEL_ONE/first_project/first_app/views.py
@@ -4,4 +4,5 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, World!")
+    my_dict = {'insert_me': "Now I am coming from first_app/index.html"}
+    return render(request, 'first_app/index.html', context=my_dict)
