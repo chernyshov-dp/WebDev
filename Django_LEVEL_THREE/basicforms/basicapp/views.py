673468ca511ac687670389759000aee25ec83d09
Daniil Chernyshov <chernyshoff.dp@gmail.com>
2021-09-07 22:11:11 +0300

    completed basic form

--- Django_LEVEL_THREE/basicforms/basicapp/views.py
+++ Django_LEVEL_THREE/basicforms/basicapp/views.py
@@ -9,4 +9,15 @@ def index(request):
 
 def form_name_view(request):
     form = forms.FormName()
+
+    if request.method == "POST":
+        form = forms.FormName(request.POST)
+
+        if form.is_valid():
+            # DO SOMETHING CODE
+            print("VALIDATION SUCCESS!")
+            print("Name -> " + form.cleaned_data['name'])
+            print("Email -> " + form.cleaned_data['email'])
+            print("Text -> " + form.cleaned_data['text'])
+
     return render(request, 'basicapp/form_page.html', {'form': form})
