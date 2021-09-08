f5c942fbe003a9e045c7201e86f16962fdbf368f
Daniil Chernyshov <chernyshoff.dp@gmail.com>
2021-09-08 20:23:15 +0300

    whole form validation

--- Django_LEVEL_THREE/basicforms/basicapp/forms.py
+++ Django_LEVEL_THREE/basicforms/basicapp/forms.py
@@ -5,7 +5,13 @@ from django.core import validators
 class FormName(forms.Form):
     name = forms.CharField()
     email = forms.EmailField()
+    verify_email = forms.EmailField(label="Enter your email again:")
     text = forms.CharField(widget=forms.Textarea)
-    botcatcher = forms.CharField(required=False,
-                                 widget=forms.HiddenInput,
-                                 validators=[validators.MaxLengthValidator(0)])
+
+    def clean(self):
+        all_clear_data = super().clean()
+        email = all_clear_data['email']
+        vmail = all_clear_data['verify_email']
+
+        if email != vmail:
+            raise forms.ValidationError("MAKE SURE EMAILS MATCH!")
