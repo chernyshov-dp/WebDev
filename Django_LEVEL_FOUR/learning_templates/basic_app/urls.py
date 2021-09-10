b206b77bd0e3af904dea32d33ae5aafb43902ef9
Daniil Chernyshov <chernyshoff.dp@gmail.com>
2021-09-10 21:18:39 +0300

    relative URLs 2/2

---
+++ Django_LEVEL_FOUR/learning_templates/basic_app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views # from basic_app import views
+
+# TEMPLATE TAGGING
+app_name = 'basic_app'
+urlpatterns = [
+    path('relative/', views.relative, name='relative'),
+    path('other/', views.other, name='other'),
+]
--- Django_LEVEL_FOUR/learning_templates/learning_templates/urls.py
+++ Django_LEVEL_FOUR/learning_templates/learning_templates/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
+from basic_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('basic_app/', include('basic_app.urls'))
 ]
