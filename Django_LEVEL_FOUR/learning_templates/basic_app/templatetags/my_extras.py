4214c970dcb7bbfc4430d2713df56eb5af4dbdc5
Daniil Chernyshov <chernyshoff.dp@gmail.com>
2021-09-10 22:35:10 +0300

    template filters

---
+++ Django_LEVEL_FOUR/learning_templates/basic_app/templatetags/my_extras.py
@@ -0,0 +1,14 @@
+from django import template
+
+register = template.Library()
+
+
+@register.filter(name='cut_it')
+def cut(value, arg):
+    """
+    This cuts out all values of "arg" from the string
+    """
+    return value.replace(arg, '')
+
+
+# register.filter('cut', cut)
--- Django_LEVEL_FOUR/learning_templates/basic_app/views.py
+++ Django_LEVEL_FOUR/learning_templates/basic_app/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'basic_app/index.html')
+    context_dict = {'text': "Hello world", 'number': 100}
+    return render(request, 'basic_app/index.html', context_dict)
 
 
 def other(request):
