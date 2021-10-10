7a6e7398c80ea1c77842875f29327418cfdf050d
Daniil Chernyshov <chernyshoff.dp@gmail.com>
2021-10-10 20:10:59 +0300

    last fixes #8

--- Projects/Blog_project/mysite/blog/models.py
+++ Projects/Blog_project/mysite/blog/models.py
@@ -7,7 +7,7 @@ class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
     text = models.TextField()
-    created_date = models.DateTimeField(default=timezone.now())
+    created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
