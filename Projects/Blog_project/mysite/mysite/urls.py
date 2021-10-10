c9ae65cf3e556ebde9b182f7b6c0c4be5ae6f227
Daniil Chernyshov <chernyshoff.dp@gmail.com>
2021-10-10 18:38:26 +0300

    fix: typos

--- Projects/Blog_project/mysite/mysite/urls.py
+++ Projects/Blog_project/mysite/mysite/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blogs.urls')),
-    path('accounts/login/', views.login, name='login'),
-    path('accounts/logout/', views.logout, name='logout', kwargs={'next_page': '/'}),
+    path('', include('blog.urls')),
+    path('accounts/login/', views.LoginView.as_view, name='login'),
+    path('accounts/logout/', views.LogoutView.as_view, name='logout', kwargs={'next_page': '/'}),
 ]
