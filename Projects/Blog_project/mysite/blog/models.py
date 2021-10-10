d80e57e37eb2b7ed8a69375eafcd40de69c59edc
Daniil Chernyshov <chernyshoff.dp@gmail.com>
2021-10-10 19:20:43 +0300

    fix: new migration #8

--- Projects/Blog_project/mysite/blog/models.py
+++ Projects/Blog_project/mysite/blog/models.py
@@ -7,7 +7,7 @@ class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
     text = models.TextField()
-    create_date = models.DateTimeField(default=timezone.now())
+    created_date = models.DateTimeField(default=timezone.now())
     published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
@@ -28,7 +28,7 @@ class Comment(models.Model):
     post = models.ForeignKey('blog.Post', related_name='comments', on_delete=models.CASCADE)
     author = models.CharField(max_length=50)
     text = models.TextField()
-    create_date = models.DateTimeField(default=timezone.now())
+    created_date = models.DateTimeField(default=timezone.now())
     approved_comment = models.BooleanField(default=False)
 
     def approve(self):
