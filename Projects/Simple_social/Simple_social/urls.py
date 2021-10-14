52a12a22c49acbbba6e0423e5416d2e3a6d5c340
Daniil Chernyshov <chernyshoff.dp@gmail.com>
2021-10-14 22:59:04 +0300

    feat: add new url patterns #11

--- Projects/Simple_social/Simple_social/urls.py
+++ Projects/Simple_social/Simple_social/urls.py
@@ -20,4 +20,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.HomePage.as_view(), name='home'),
+    path('accounts/', include('accounts.urls', namespace='accounts')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
