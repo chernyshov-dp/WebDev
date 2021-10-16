c919a2774520199d0996f8662965226d675f0a46
Daniil Chernyshov <chernyshoff.dp@gmail.com>
2021-10-16 23:48:16 +0300

    fix: views.SignUp #11

--- Projects/Simple_social/accounts/urls.py
+++ Projects/Simple_social/accounts/urls.py
@@ -7,5 +7,5 @@ app_name = 'accounts'
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('signup/', auth_views.SignUpView.as_view(), name='signup'),
+    path('signup/', views.SignUp.as_view(), name='signup'),
 ]
