92bd674391b0a66a4121e19f92c0305196e51aab
Daniil Chernyshov <chernyshoff.dp@gmail.com>
2021-10-21 23:15:13 +0300

    feat: add groups views #11

--- Projects/Simple_social/groups/views.py
+++ Projects/Simple_social/groups/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.urls import reverse
+from django.views import generic
 
-# Create your views here.
+from groups.models import Group, GroupMember
+
+
+class CreateGroup(LoginRequiredMixin, generic.CreateView):
+    fields = ('name', 'description')
+    model = Group
+
+
+class SingleGroup(generic.DetailView):
+    model = Group
+
+
+class ListGroups(generic.ListView):
+    model = Group
