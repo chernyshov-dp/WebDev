7ba625a1fc15f31f924a296554e7f218a0449dc4
Daniil Chernyshov <chernyshoff.dp@gmail.com>
2021-10-24 00:20:19 +0300

    feat: registered the Groups models #11

--- Projects/Simple_social/groups/admin.py
+++ Projects/Simple_social/groups/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+class GroupMemberInLine(admin.TabularInline):
+    model = models.GroupMember
+
+
+admin.site.register(models.Group)
