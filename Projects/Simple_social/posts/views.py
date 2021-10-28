adaafb22912ef64ed6817e03e2fc81aed3271507
Daniil Chernyshov <chernyshoff.dp@gmail.com>
2021-10-28 23:55:18 +0300

    feat: add PostList, UserPosts, PostDetail #11

--- Projects/Simple_social/posts/views.py
+++ Projects/Simple_social/posts/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 
-# Create your views here.
+from django.http import Http404
+from django.views import generic
+
+from braces.views import SelectRelatedMixin
+
+from . import models
+from . import forms
+
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
+
+
+class PostList(SelectRelatedMixin, generic.ListView):
+    model = models.Post
+    select_related = ('user', 'group')
+
+
+class UserPosts(generic.ListView):
+    model = models.Post
+    template_name = 'posts/user_post_list.html'
+
+    def get_queryset(self):
+        try:
+            self.post.user = User.objects.prefetch_related('posts').get(username__iexact=self.kwargs.get('username'))
+
+        except User.DoesNotExist:
+            raise Http404
+
+        else:
+            return self.post_user.posts.all()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['post_user'] = self.post_user
+        return context
+
+
+class PostDetail(SelectRelatedMixin, generic.DetailView):
+    model = models.Post
+    select_related = ('user', group)
+    
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(user__username__iexact=self.kwargs.get('username'))
