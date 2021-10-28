0c06abf88f88ad596aed5451c439197e64fca3e9
Daniil Chernyshov <chernyshoff.dp@gmail.com>
2021-10-29 00:02:41 +0300

    feat: add CreatePost and DeletePost #11

--- Projects/Simple_social/posts/views.py
+++ Projects/Simple_social/posts/views.py
@@ -43,7 +43,32 @@ class UserPosts(generic.ListView):
 class PostDetail(SelectRelatedMixin, generic.DetailView):
     model = models.Post
     select_related = ('user', group)
-    
+
     def get_queryset(self):
         queryset = super().get_queryset()
         return queryset.filter(user__username__iexact=self.kwargs.get('username'))
+
+
+class CreatePost(LoginRequiredMixin, SelectRelatedMixin, generic.CreateView):
+    fields = ('message', 'group')
+    model = models.Post
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        return super().form_valid(form)
+
+
+class DeletePost(LoginRequiredMixin, SelectRelatedMixin, generic.DeleteView):
+    model = models.Post
+    select_related = ('user', 'group')
+    success_url = reverse_lazy('posts:all')
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset(user_id=self.request.user.id)
+
+    def delete(self, *args, **kwargs):
+        messages.success(self.request, 'Post deleted')
+        return super().delete(*args, **kwargs)
