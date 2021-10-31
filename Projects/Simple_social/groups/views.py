08efb4c2d687912e0bebea6da6a5d4e1e5d41061
Daniil Chernyshov <chernyshoff.dp@gmail.com>
2021-10-31 23:59:48 +0300

    feat: add JoinGroup class 1/2 #11

--- Projects/Simple_social/groups/views.py
+++ Projects/Simple_social/groups/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.urls import reverse
 from django.views import generic
+from django.shortcuts import get_object_or_404
 
 from groups.models import Group, GroupMember
 
@@ -17,3 +19,17 @@ class SingleGroup(generic.DetailView):
 
 class ListGroups(generic.ListView):
     model = Group
+
+
+class JoinGroup(LoginRequiredMixin, generic.RedirectView):
+
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse('groups:single', kwargs={'slug': self.kwargs.get('slug')})
+
+    def get(self, request, *args, **kwargs):
+        group = get_object_or_404(Group, slug=self.kwargs.get('slug'))
+
+        try:
+            GroupMember.object.create(user=self.request.user, group=group)
+        except IntegrityError:
+            messa
