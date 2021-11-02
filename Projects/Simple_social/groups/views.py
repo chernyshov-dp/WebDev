7953cef750617581d391332459fdc0c47ed92426
Daniil Chernyshov <chernyshoff.dp@gmail.com>
2021-11-02 22:41:55 +0300

    feat: add LeaveGroup class #11

--- Projects/Simple_social/groups/views.py
+++ Projects/Simple_social/groups/views.py
@@ -37,3 +37,23 @@ class JoinGroup(LoginRequiredMixin, generic.RedirectView):
             messages.success(self.request, 'You are now a member!')
 
         return super().get(request, *args, **kwargs)
+
+
+class LeaveGroup(LoginRequiredMixin, generic.RedirectView):
+
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse('groups:single', kwargs={'slug': self.kwargs.get('slug')})
+
+    def get(self, request, *args, **kwargs):
+        try:
+            membership = models.GroupMember.object.filter(
+                user=self.request.user,
+                group__slug=self.kwargs.get('slug')
+            ).get()
+        except models.GroupMember.DoesNotExist:
+            messages.warning(self.request, 'Sorry you are not in this group!')
+        else:
+            membership.delete()
+            messages.success(self.request, 'You have left the group!')
+
+        return super().get(request, *args, **kwargs)
