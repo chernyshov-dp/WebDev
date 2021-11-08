f0ea7cb7aa02e9ff39acff35abc65378b598ec42
Daniil Chernyshov <chernyshoff.dp@gmail.com>
2021-11-08 21:08:57 +0300

    fix: post_user typo in posts views #11

--- Projects/Simple_social/posts/views.py
+++ Projects/Simple_social/posts/views.py
@@ -26,7 +26,7 @@ class UserPosts(generic.ListView):
 
     def get_queryset(self):
         try:
-            self.post.user = User.objects.prefetch_related('posts').get(username__iexact=self.kwargs.get('username'))
+            self.post_user = User.objects.prefetch_related('posts').get(username__iexact=self.kwargs.get('username'))
 
         except User.DoesNotExist:
             raise Http404
