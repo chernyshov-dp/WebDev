32ebc5e96c8d474edf48f122902ead1dbcaaf5df
Daniil Chernyshov <chernyshoff.dp@gmail.com>
2021-11-21 23:53:32 +0300

    feat: adding search #13

--- Django_admin/my_video_rental/videos/admin.py
+++ Django_admin/my_video_rental/videos/admin.py
@@ -4,6 +4,7 @@ from . import models
 
 class MovieAdmin(admin.ModelAdmin):
     fields = ['release_year', 'title', 'length']
+    search_fields = ['title', 'length']
 
 
 admin.site.register(models.Customer)
