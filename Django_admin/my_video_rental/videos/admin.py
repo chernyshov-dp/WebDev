d86610bece892bdefb454b1a8e13742058dd349a
Daniil Chernyshov <chernyshoff.dp@gmail.com>
2021-11-21 23:55:20 +0300

    feat: adding filters #13

--- Django_admin/my_video_rental/videos/admin.py
+++ Django_admin/my_video_rental/videos/admin.py
@@ -5,6 +5,7 @@ from . import models
 class MovieAdmin(admin.ModelAdmin):
     fields = ['release_year', 'title', 'length']
     search_fields = ['title', 'length']
+    list_filter = ['release_year', 'length']
 
 
 admin.site.register(models.Customer)
