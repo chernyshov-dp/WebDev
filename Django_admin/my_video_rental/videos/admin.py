6e5ba9facbae531a23fa17a78ce4d9fd97a1ecd9
Daniil Chernyshov <chernyshoff.dp@gmail.com>
2021-11-21 23:56:59 +0300

    feat: adding fields #13

--- Django_admin/my_video_rental/videos/admin.py
+++ Django_admin/my_video_rental/videos/admin.py
@@ -6,6 +6,7 @@ class MovieAdmin(admin.ModelAdmin):
     fields = ['release_year', 'title', 'length']
     search_fields = ['title', 'length']
     list_filter = ['release_year', 'length']
+    list_display = ['title', 'release_year', 'length']
 
 
 admin.site.register(models.Customer)
