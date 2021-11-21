690c7e73d095e9ee4f124f18e071971442a23524
Daniil Chernyshov <chernyshoff.dp@gmail.com>
2021-11-21 23:50:31 +0300

    feat: ordering fields #13

--- Django_admin/my_video_rental/videos/admin.py
+++ Django_admin/my_video_rental/videos/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from . import models
 
+
+class MovieAdmin(admin.ModelAdmin):
+    fields = ['release_year', 'title', 'length']
+
+
 admin.site.register(models.Customer)
-admin.site.register(models.Movie)
+admin.site.register(models.Movie, MovieAdmin)
